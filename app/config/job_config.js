731b0dc90cbb420494ae583e900548f07d533f63
suraj patil <89598901+surajpatil510@users.noreply.github.com>
2022-08-14 22:48:53 +0530

    update

--- app/config/job_config.js
+++ app/config/job_config.js
@@ -1,6 +1,6 @@
 module.exports={
     JOBS_SCHEDULE : {
         "TRANSACTIONS_JOB": "15 14 * * *",
-        "ETHEREUM_PRICE_JOB": "*/10 * * * * *"
+        "ETHEREUM_PRICE_JOB": "*/10 * * * *"
     }
 }
