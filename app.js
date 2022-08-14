cadf485f988bc392daa043f3bf884d471a29f8d7
suraj patil <89598901+surajpatil510@users.noreply.github.com>
2022-08-14 22:40:29 +0530

    fix

--- app.js
+++ app.js
@@ -3,11 +3,11 @@ const express = require("express");
 const app = express();
 const serviceLocator = require("./app/lib/service_locator");
 const route = require("./start/routes");
-const Database = serviceLocator.get("Database");
 const PORT = process.env.PORT || 5000;
 const errorHandler = serviceLocator.get("errorHandler");
 const jobServer = require("./job_server");
 const bodyParser = require("body-parser");
+const Database = serviceLocator.get("Database");
 
 // jobServer();
 
